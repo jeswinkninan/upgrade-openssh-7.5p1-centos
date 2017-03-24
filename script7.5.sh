b2d0babcce7cda2da64af62babd93b4f3e358d2d
JESWINKNINAN <jeswinjkn@gmail.com>
2017-03-25 04:01:09 +0530

    Update script7.5.sh

--- script7.5.sh
+++ script7.5.sh
@@ -1,4 +1,6 @@
 #/bin/bash
+baktime=$(date +%s)
+p /etc/pam.d/sshd pam-ssh-conf-$baktime
 wget http://ftp.spline.de/pub/OpenBSD/OpenSSH/portable/openssh-7.5p1.tar.gz
 tar -xvf openssh-7.5p1.tar.gz && cd openssh-7.5p1
 ./configure && make && make install
