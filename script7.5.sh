c886276a741f740a70d4ec377bc44f15e2a16e04
jeswin <JESWINKNINAN>
2017-03-25 03:54:06 +0530

    script for non-rpm install/compile install

---
+++ script7.5.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+wget http://ftp.spline.de/pub/OpenBSD/OpenSSH/portable/openssh-7.5p1.tar.gz
+tar -xvf openssh-7.5p1.tar.gz && cd openssh-7.5p1
+./configure && make && make install
+echo "New version upgrades as to lastest:" && $(ssh -V) 
